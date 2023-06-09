2caad76c333fbcabb52b8050650e717936c3a56a
Igor Simakov <i.simakov@banki.ru>
2023-06-09 10:09:06 +0800

    spring-boot-maven-plugin consumer

---
+++ src/main/java/ru/simakov/listener/NotificationConsumer.java
@@ -0,0 +1,21 @@
+package ru.simakov.listener;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import ru.simakov.clients.notification.NotificationRequest;
+import ru.simakov.service.NotificationService;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationConsumer {
+    private final NotificationService notificationService;
+
+    @RabbitListener(queues = "${rabbitmq.queue.notification}")
+    public void consumer(NotificationRequest notificationRequest) {
+        log.info("Consumed message: {}", notificationRequest);
+        notificationService.sendNotification(notificationRequest);
+    }
+}
